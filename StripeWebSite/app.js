807a5e6ef1dbd50890ed7d9c6260a059ee17874b
Timur <ismajlov47@gmail.com>
2024-03-10 16:05:03 +0600

    Stripe

---
+++ StripeWebSite/app.js
@@ -0,0 +1,4 @@
+const heroTitle = document.querySelector('.hero-title');
+const heroTitleOverlay = document.querySelector('.hero-title[data-overlay]');
+
+heroTitleOverlay.innerText = heroTitle.textContent;
